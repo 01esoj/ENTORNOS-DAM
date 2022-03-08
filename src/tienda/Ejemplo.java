c2a358c4f46cb9a0974619e4caf5bdd3345d097b
IEUser <IEUser@10.0.2.15>
2022-03-08 20:20:26 +0100

    Commit 3

--- src/tienda/Ejemplo.java
+++ src/tienda/Ejemplo.java
@@ -12,6 +12,10 @@ public class Ejemplo {
 		int num2 = 69;
 		
 		System.out.println("El segundo numero es "+num2);
+		
+		int num3 = 15;
+		
+		System.out.println("El tercer numero es "+num3);
 	}
 
 }
